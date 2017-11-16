d0d207814adb9afc4e14321cb49bb8b18ac7f946
Jonathan De Leon <jondeleon200@gmail.com>
2017-11-15 20:33:58 -0800

    Added comments and error checking. Cleaned up code.

--- src/sortMerge.C
+++ src/sortMerge.C
@@ -11,10 +11,6 @@ static const char* ErrMsgs[] = 	{};
 
 static error_string_table ErrTable( JOINS, ErrMsgs );
 
-struct _rec {
-    int	key;
-    char	filler[4];
-};
 sortMerge::sortMerge(
     char*           filename1,      // Name of HeapFile for relation R
     int             len_in1,        // # of columns in R.
@@ -33,55 +29,90 @@ sortMerge::sortMerge(
     TupleOrder      order,          // Sorting order: Ascending or Descending
     Status&         s               // Status of constructor
 ){
+    // Creating heap files and sorting them while checking for any status errors
     char* filename1_sorted = "sorted1";
     char* filename2_sorted = "sorted2";
     HeapFile* sorted1 = new HeapFile(filename1_sorted, s);
+    if(s != OK){
+         MINIBASE_CHAIN_ERROR(JOINS,s);
+         return;
+    }
     HeapFile* sorted2 = new HeapFile(filename2_sorted, s);
+    if(s != OK){
+         MINIBASE_CHAIN_ERROR(JOINS,s);
+         return;
+    }
     HeapFile* outFile = new HeapFile(filename3, s);
-    Sort* sort1 = new Sort(filename1, filename1_sorted, len_in1, in1, t1_str_sizes, join_col_in1, order, amt_of_mem, s);
-    Sort* sort2 = new Sort(filename2, filename2_sorted, len_in2, in2, t2_str_sizes, join_col_in2, order, amt_of_mem, s);
+    if(s != OK){
+         MINIBASE_CHAIN_ERROR(JOINS,s);
+         return;
+    }
+    Sort(filename1, filename1_sorted, len_in1, in1, t1_str_sizes, join_col_in1, order, amt_of_mem, s);
+    if(s != OK){
+         MINIBASE_CHAIN_ERROR(JOINS,s);
+         return;
+    }
+    Sort(filename2, filename2_sorted, len_in2, in2, t2_str_sizes, join_col_in2, order, amt_of_mem, s);
+    if(s != OK){
+         MINIBASE_CHAIN_ERROR(JOINS,s);
+         return;
+    }
 
+    // Initialize our scanners for a sorted HeapFil
     Scan* scan1 = sorted1->openScan(s);
+    if(s != OK){
+         MINIBASE_CHAIN_ERROR(JOINS,s);
+         return;
+    }
     Scan* scan2 = sorted2->openScan(s);
+    if(s != OK){
+         MINIBASE_CHAIN_ERROR(JOINS,s);
+         return;
+    }
 
-    RID rid;
-    RID rid1;
-    RID rid2;
-    RID ridTmp;
+    RID rid;    // RID for output file
+    RID rid1;   // RID cursor position in relation R
+    RID rid2;   // RID cursor position in relation S
+    RID ridTmp; // RID cursor tracker position in relation S
 
+    // Retrieve size of a record in R
     int recsize1 = 0;
     for(int i = 0; i < len_in1; i++) {
         recsize1 += t1_str_sizes[i];
     }
 
+    // Retrieve size of a record in S
     int recsize2 = 0;
     for(int i = 0; i < len_in2; i++) {
         recsize2 += t2_str_sizes[i];
     }
 
+    // Initialize memory to temporarily store a record
     char* rec1 = new char[recsize1];
-
     char* rec2 = new char[recsize2];
+    char* recResult = new char[recsize1+recsize2];
 
-    char* recResult = new char[recsize1+recsize2+1];
-
-
+    /**
+      Sort-merge Join Algorithm 
+      Note: To verbosely view algorithm, uncomment the three
+            'cout' lines 
+     **/
     s = scan1->getNext(rid1, rec1, recsize1);
-
     if(s == OK) {
         s = scan2->getNext(rid2, rec2, recsize2);
         ridTmp = rid2;
     }
-    char* rec[len_in1+len_in2];
     while(s == OK) {
-        if(*(int*)(rec1) == *(int*)(rec2)) {
+        // tupleCmp from 'sort.C' 
+        // returns 0 if equal, 1 if greater than, -1 if less than
+        if(tupleCmp(rec1, rec2) == 0) {
            // cout << *(int*)(rec1) << " == " << *(int*)(rec2) << endl;
-            memcpy(recResult, &rec1[0], recsize1);
-            memcpy(recResult+recsize1, &rec2[0], recsize2);
+            memcpy(recResult, rec1, recsize1);              // memcpy moves rec1 && rec2 into recResult
+            memcpy(recResult+recsize1, rec2, recsize2);
             s = outFile->insertRecord(recResult, recsize1+recsize2, rid);
             s = scan2->getNext(rid2, rec2, recsize2);
         }
-        else if(*(int*)(rec1) > *(int*)(rec2))  {
+        else if(tupleCmp(rec1, rec2) == 1) {
            // cout << *(int*)(rec1) << " > " << *(int*)(rec2) << endl;
             s = scan2->getNext(rid2, rec2, recsize2);
             ridTmp = rid2;
@@ -92,17 +123,14 @@ sortMerge::sortMerge(
             if(s == OK) {
                 s = scan2->position(ridTmp);
                 s = scan2->getNext(rid2, rec2, recsize2);
-                s = OK;
-            } else {
-                s = scan2->getNext(rid2, rec2, recsize2);
-            }
+                s = OK;     // If we are at the end of scan2 but not scan1 keep going
+            } 
         }
     }
 
-
+    // Destruct scanners and HeapFiles to deallocate memory
     delete scan1;
     s = sorted1->deleteFile();
-
     delete scan2;
     s = sorted2->deleteFile();
 }
