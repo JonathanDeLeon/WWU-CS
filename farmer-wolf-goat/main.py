6c55468df633fe7d0c4afc4831aecc37b068aab6
Jonathan De Leon <jondeleon200@gmail.com>
2018-04-15 17:39:24 -0700

    Add Puzzle state

--- farmer-wolf-goat/main.py
+++ farmer-wolf-goat/main.py
@@ -11,18 +11,88 @@
 # Your goal is to transfer everyone to the other side of the river safely.
 
 
-class EntityState:
+class Entity:
     WEST, EAST = 0, 1
 
-    def __init__(self, name=None, state=None):
-        self.name = name
+    def __init__(self, state=None):
         self.state = state or self.WEST
 
-    def isEast(self):
+    def is_east(self):
         return self.state == self.EAST
 
-    def isWest(self):
+    def is_west(self):
         return self.state == self.WEST
 
+    def __eq__(self, other):
+        return self.state == other.state
+
     def __str__(self):
-        return (str(self.name), "WEST" if self.state == self.WEST else "EAST")
+        return "WEST" if self.state == self.WEST else "EAST"
+
+
+class PuzzleState:
+    def __init__(self, state=None):
+        self.state = state
+
+    def empty(self):
+        if not self.state:
+            return True
+        return False
+
+    def valid_state(self):
+        '''
+        :return True if farmer is with goat or if goat is not alone with wolf or cabbage
+        '''
+        if self.empty():
+            return False
+
+        try:
+            if self.state['farmer'] == self.state['goat']:
+                return True
+            elif self.state['goat'] == self.state['wolf']:
+                return False
+            elif self.state['goat'] == self.state['cabbage']:
+                return False
+        except:
+            return False
+        else:
+            return True
+
+    def goal(self):
+        '''
+        :return True if all entities in state are east of the river
+        '''
+        if self.empty():
+            return False
+        try:
+            for entity in self.state.values():
+                if entity.is_west:
+                    return False
+        except:
+            return False
+        else:
+            return True
+
+    def __eq__(self, other):
+        return cmp(self.state, other.state)
+
+    def __str__(self):
+        return str(self.state)
+
+
+def Puzzle:
+    initial = {'farmer': Entity(), 'wolf': Entity(), 'goat': Entity(), 'cabbage': Entity()}
+    path = []
+
+    def __init__(self):
+        self.path.append(PuzzleState(initial))
+
+    def find_solution(self):
+        next_state = self.path[-1].copy()
+        while next_state and not next_state.goal():
+
+
+
+
+
+puzzle = PuzzleState()
