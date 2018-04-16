8ab536083a2c5b619b79717f1c5c2fb9bc8573b5
Jonathan De Leon <jonathan@campgroundautomation.com>
2018-04-16 15:46:03 -0700

    Add hw01

--- hw01/Circle.h
+++ hw01/Circle.h
@@ -1,14 +1,39 @@
 /*************************************************************************
  *
  * HW01: Building a basic shape and square class to highlight C++ concepts.
- *      - Polymorphis
+ *      - Polymorphism
  *      - Inheritance
  *      - Exception handling
  * 
  * File Name:  Circle.h
- * Name:       ?
+ * Name:       Jonathan De Leon
  * Course:     CPTR 242
- * Date:       ?
- * Time Taken: ?
+ * Date:       April 16, 2018
+ * Time Taken: 1 hour
  * 
  */
+
+#ifndef circle_h
+#define circle_h
+
+#include <iostream>
+#include "Shape.h"
+
+using namespace std;
+
+class Circle : public Shape {
+private:
+    int radius;
+public:
+    Circle();
+
+    Circle(int radius);
+
+    int getRadius() const;
+
+    friend ostream &operator<<(ostream &output, const Circle &shape);
+};
+
+Circle getCircleFromUser();
+
+#endif // CIRCLE_H
