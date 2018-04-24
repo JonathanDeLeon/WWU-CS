dc66d2c426545a0fcc1b02fe20e2d47b8f8a3bf1
Jonathan De Leon <jondeleon200@gmail.com>
2018-04-23 23:45:04 -0700

    Update README; Add branch and bound solutions

--- three-puzzle/README.md
+++ three-puzzle/README.md
@@ -4,10 +4,16 @@ Assignment #3 for *CPTR 430 Artificial Intelligence*.
 
 ## Problem
 
+The goal of this assignment was to write a program to solve the 3-Puzzle Tile in the following ways:
+* Using BFS
+* Branch and Bound with past cost (depth)
+* Branch and Bound with simple heuristic (number of tiles out of place) and past cost
+* Branch and Bound with refined heuristic (sum of moves needed for each tile) and past cost
+* Branch and Bound with past cost, refined heuristic, and dynamic programming (A* Search)
+
 ## Getting Started
 
-This assignment was done in python 3.6. It is also implemented using a BFS strategy (queue with FIFO operations),
-Branch and Bound.
+This assignment was done in python 3.6.
 
 ```python
 python3 main.py
@@ -15,6 +21,6 @@ python3 main.py
 
 ## Output
 
-### Sample
-```python
-```
+Program will output 5 different solutions to the problem using the 5 different implementations.
+The goal state of the puzzle is `[1, 2, 3, None]` where *None* is the location of the free "move-able tile".
+The program will ask for user input to continue on with the next algorithm until all solutions have been displayed.
--- three-puzzle/main.py
+++ three-puzzle/main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # Jonathan De Leon
 # CPTR 430 Artificial Intelligence
 # Homework 3
@@ -14,32 +14,33 @@ class PuzzleState:
     Class to represent the state of the 3-Puzzle
     '''
 
-    def __init__(self, state, prev_states=None, cost=None):
+    def __init__(self, state, prev_states=None, cost_method=None):
         self.state = state
         self.free_tile = self.state.index(None)
         self.width = 2  # 2x2 board; total of 4 elements
         self.prev_states = prev_states or []
-        self.cost = cost
+        self.cost = cost_method(self.state) if cost_method is not None else None
 
-    def expand_children(self):
+    def expand_children(self, cost_method=None):
         '''
         Yield a python generator with expanded children states
         '''
+        parents = self.prev_states + [self]
         # Move tile up
         if self.free_tile - self.width >= 0:
-            yield PuzzleState(self.move(self.free_tile - self.width), self.prev_states + [self])
+            yield PuzzleState(self.move(self.free_tile - self.width), parents, cost_method)
 
         # Move tile down
         if self.free_tile + self.width < len(self.state):
-            yield PuzzleState(self.move(self.free_tile + self.width), self.prev_states + [self])
+            yield PuzzleState(self.move(self.free_tile + self.width), parents, cost_method)
 
         # Move tile left if row numbers are equal
         if (self.free_tile - 1) // self.width == (self.free_tile) // self.width:
-            yield PuzzleState(self.move(self.free_tile - 1), self.prev_states + [self])
+            yield PuzzleState(self.move(self.free_tile - 1), parents, cost_method)
 
         # Move tile right if row numbers are equal
         if (self.free_tile + 1) // self.width == (self.free_tile) // self.width:
-            yield PuzzleState(self.move(self.free_tile + 1), self.prev_states + [self])
+            yield PuzzleState(self.move(self.free_tile + 1), parents, cost_method)
 
     def move(self, dest_tile):
         '''
@@ -63,8 +64,11 @@ class PuzzleState:
         '''
         Pretty print the state of the puzzle
         '''
-        return "\n".join(str(self.state[start: start + self.width])
-                         for start in range(0, len(self.state), self.width))
+        to_string = "\n".join(str(self.state[start: start + self.width])
+                              for start in range(0, len(self.state), self.width))
+        if self.cost is not None:
+            to_string += "\nCost||Heuristic: " + str(self.cost)
+        return to_string
 
     def __eq__(self, other):
         '''
@@ -93,7 +97,7 @@ class PuzzleSolver:
     def __init__(self, initial):
         self.initial = initial
 
-    def solve(self):
+    def solve_BFS(self):
         '''
         Use BFS and return solution PuzzleState
         '''
@@ -110,22 +114,163 @@ class PuzzleSolver:
                 if puzzle not in visited and puzzle not in queue:
                     queue.appendleft(puzzle)
 
+    def solve_uniform_cost_search(self):
+        '''
+        Use Uniform Cost Search and return solution PuzzleState
+        Cost is the number of steps needed to get to the state
+        '''
+        queue = collections.deque([self.initial])
+        visited = set()
+
+        while queue:
+            node = queue.pop()
+            visited.add(node)
+            if node.goal:
+                return node
+
+            for puzzle in node.expand_children(cost_method=lambda state: depth(len(node.prev_states))):
+                if puzzle not in visited and puzzle not in queue:
+                    queue.appendleft(puzzle)
+            # Sort queue by cost
+            sorted(queue, key=lambda state: state.cost, reverse=True)
+
+    def solve_simple_heuristic(self):
+        '''
+        Use Branch and Bound and return solution PuzzleState
+        Cost is the number of steps needed to get to the state plus number of tiles out of place
+        '''
+        queue = collections.deque([self.initial])
+        visited = set()
+
+        while queue:
+            node = queue.pop()
+            visited.add(node)
+            if node.goal:
+                return node
+
+            for puzzle in node.expand_children(
+                    cost_method=lambda state: depth(len(node.prev_states)) + simple_heuristic(state)):
+                if puzzle not in visited and puzzle not in queue:
+                    queue.appendleft(puzzle)
+            # Sort queue by cost
+            sorted(queue, key=lambda state: state.cost, reverse=True)
+
+    def solve_complex_heuristic(self):
+        '''
+        Use Branch and Bound and return solution PuzzleState
+        Cost is the number of steps needed to get to the state plus sum of moves needed for each tile
+        '''
+        queue = collections.deque([self.initial])
+        visited = set()
+
+        while queue:
+            node = queue.pop()
+            visited.add(node)
+            if node.goal:
+                return node
+
+            for puzzle in node.expand_children(
+                    cost_method=lambda state: depth(len(node.prev_states)) + complex_heuristic(state)):
+                if puzzle not in visited and puzzle not in queue:
+                    queue.appendleft(puzzle)
+            # Sort queue by cost
+            sorted(queue, key=lambda state: state.cost, reverse=True)
+
+    def solve_dynamic_programming(self):
+        '''
+        Use A* Search or Branch and Bound with Dynamic Programming and return solution PuzzleState
+        Cost is the number of steps needed to get to the state and sum of moves needed for each tile
+        '''
+        queue = collections.deque([self.initial])
+        visited = set()
+
+        while queue:
+            node = queue.pop()
+            visited.add(node)
+            if node.goal:
+                return node
+
+            for puzzle in node.expand_children(
+                    cost_method=lambda state: depth(len(node.prev_states)) + complex_heuristic(state)):
+                if puzzle not in visited and puzzle not in queue:
+                    queue.appendleft(puzzle)
+            # Sort queue by cost
+            sorted(queue, key=lambda state: state.cost, reverse=True)
+
+
+def depth(distance):
+    '''
+    distance from start node
+    '''
+    return 1 + (distance if distance is not None else 0)
+
+
+def simple_heuristic(state):
+    '''
+    heuristic: number of tiles out of places
+    '''
+    h = 0
+    for i, v in enumerate(state):
+        if v is None:
+            continue
+        if i != (v - 1):
+            h += 1
+    return h
+
+
+def complex_heuristic(state):
+    '''
+    heuristic: sum of moves needed for each tile
+    calculate Manhattan distance
+    '''
+    # dictionary of { value : index }
+    goal = {1: 0, 2: 1,
+            3: 2, None: 3}
+    width = 2
+    h = 0
+    for i, v in enumerate(state):
+        if v is None:
+            continue
+        if i != goal.get(v, i):
+            # |x1 - x2| + |y1 - y2|
+            h += abs((i % width) - (goal.get(v) % width)) + abs((i // width) - (goal.get(v) // width))
+    return h
+
+
+def pretty_print_children(puzzle):
+    '''
+    pretty print puzzle with all of its children
+    '''
+    if puzzle is not None:
+        for state in puzzle.prev_states:
+            print(state, end='\n\n')
+    print(puzzle)
+
 
 if __name__ == "__main__":
     # Use initial state found in book 
     puzzle = PuzzleState([3, 1, 2, None])
-    solution = PuzzleSolver(puzzle).solve()
-    if solution is not None:
-        for state in solution.prev_states:
-            print(state, end='\n\n')
-    print(solution)
-    # print("\n====================================")
-    # print("Initial root node")
-    # print(puzzle.initial)
-    # print("====================================")
-    # print("Solution")
-    # print(puzzle.find_solution())
-    # print("====================================")
-    # print("List of visited nodes before finding solution")
-    # print(puzzle.visited)
-    # print("====================================", end='\n\n')
+    print("\n====================================")
+    print("Initial Puzzle State")
+    print(puzzle)
+    print("====================================")
+    print("BFS Solution")
+    solution = PuzzleSolver(puzzle).solve_BFS()
+    pretty_print_children(solution)
+    print("====================================")
+    print("Branch and Bound with Cost")
+    solution = PuzzleSolver(puzzle).solve_uniform_cost_search()
+    pretty_print_children(solution)
+    print("====================================")
+    print("Branch and Bound with Simple Heuristic")
+    solution = PuzzleSolver(puzzle).solve_simple_heuristic()
+    pretty_print_children(solution)
+    print("====================================")
+    print("Branch and Bound with Complex Heuristic")
+    solution = PuzzleSolver(puzzle).solve_complex_heuristic()
+    pretty_print_children(solution)
+    print("====================================")
+    print("Branch and Bound with Dynamic Programming")
+    solution = PuzzleSolver(puzzle).solve_dynamic_programming()
+    pretty_print_children(solution)
+    print("====================================", end='\n\n')
