bb67183f88bc099644134a11b2068adddebf41d8
Jonathan De Leon <jonathan.deleon@wallawalla.edu>
2018-05-28 12:28:22 -0700

    Finish homework 7

--- hw07/ancestors.cpp
+++ hw07/ancestors.cpp
@@ -3,22 +3,46 @@
  * Howemork.06: Implement a three functions for printing tree ancestors.
  *
  * File Name:  ancestors.cpp
- * Name:       ?
+ * Name:       Jonathan De Leon
  * Course:     CPTR 242
- * Date:       ?
- * Time Taken: ?
+ * Date:       5/28/18
+ * Time Taken: 1 hr
  *
  */
 #include "TreeType.h"
 
 void TreeType::AncestorsIterative(ItemType value, std::ofstream& outFile) {
-    // TODO implement ancestors iterative print
+    TreeNode* tree = root;
+    while (tree != NULL && tree->info != value) {
+        outFile << tree->info;
+        if (value < tree->info) {
+            tree = tree->left;
+        } else if (value > tree->info) {
+            tree = tree->right;
+        }
+    }
 }
 
 void PrintAncestorsRecursive(TreeNode* tree, ItemType value, std::ofstream& outFile) {
-    // TODO implement ancestors recursive print
+    if (tree == NULL || tree->info == value) {
+        return;
+    }
+    outFile << tree->info;
+    if (value < tree->info) {
+        PrintAncestorsRecursive(tree->left, value, outFile);
+    } else if (value > tree->info) {
+        PrintAncestorsRecursive(tree->right, value, outFile);
+    }
 }
 
 void PrintAncestorsReverse(TreeNode* tree, ItemType value, std::ofstream& outFile) {
-    // TODO implement ancestors reverse print
+    if (tree == NULL || tree->info == value) {
+        return;
+    }
+    if (value < tree->info) {
+        PrintAncestorsReverse(tree->left, value, outFile);
+    } else if (value > tree->info) {
+        PrintAncestorsReverse(tree->right, value, outFile);
+    }
+    outFile << tree->info;
 }
