9abc9c06b637e6e952331a6c49c16797f1948373
Jonathan De Leon <jonathan.deleon@wallawalla.edu>
2018-05-30 00:00:37 -0700

    Update test driver with new date models

--- proj02/test_driver.cpp
+++ proj02/test_driver.cpp
@@ -3,21 +3,21 @@
 //
 
 #include <iostream>
-#include <assert.h>
+#include <cassert>
 #include "PQType.h"
-#include "Applicant.h"
+#include "Application.h"
 
 /*
  * Return true if queue has its priorities maintained in the data structure
  */
-bool TestPriorityMaintained(PQType<Applicant> queue) {
+bool TestPriorityMaintained(PQType<Application> queue) {
     if (queue.IsEmpty()) {
         // Empty queue maintains order
         return true;
     }
 
-    Applicant prev;
-    Applicant current;
+    Application prev;
+    Application current;
 
     queue.Dequeue(prev);
     if (queue.IsEmpty()) {
@@ -29,7 +29,7 @@ bool TestPriorityMaintained(PQType<Applicant> queue) {
     // Recall: Priority queue dequeues highest priority in FIFO fashion
     while (!queue.IsEmpty()) {
         queue.Dequeue(current);
-        if (prev.ComparedTo(current) == LESS) {
+        if (prev < current) {
             return false;
         }
         prev = current;
@@ -38,18 +38,19 @@ bool TestPriorityMaintained(PQType<Applicant> queue) {
 }
 
 /*
- * Test inserting applicant to an empty queue
+ * Test inserting application to an empty queue
  */
 void TestEnqueueOnEmpty() {
-    PQType<Applicant> queue(2);
+    PQType<Application> queue(2);
+    Application application;
     Applicant applicant;
 
     // Check if initial queue is empty
     assert(queue.IsEmpty());
 
-    // Insert one applicant into queue
-    applicant.Initialize(10);
-    queue.Enqueue(applicant);
+    // Insert one application into queue
+    application.Initialize(applicant);
+    queue.Enqueue(application);
     assert(!queue.IsEmpty());
 }
 
@@ -57,70 +58,95 @@ void TestEnqueueOnEmpty() {
  * Test general insertion to a priority queue
  */
 void TestEnqueueGeneral() {
-    PQType<Applicant> queue(3);
+    PQType<Application> queue(4);
+    Application application;
     Applicant applicant;
 
     // Add applicants to the queue
-    applicant.Initialize(10);
-    queue.Enqueue(applicant);
-    applicant.Initialize(5);
-    queue.Enqueue(applicant);
-    applicant.Initialize(8);
-    queue.Enqueue(applicant);
+    applicant.Initialize(1, "Test 1", 18, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    applicant.Initialize(1, "Test 2 Priority", 25, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    applicant.Initialize(1, "Test 3", 17, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    applicant.Initialize(1, "Test 4 Priority", 17, true, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    applicant.Initialize(1, "Test 5 Priority", 18, false, 3);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
 
     // Check if priority is maintained in the data structure
     assert(TestPriorityMaintained(queue));
 }
 
 /*
- * Test special case when removing last applicant
+ * Test special case when removing last application
  */
 void TestDequeueOneApplicant() {
-    PQType<Applicant> queue(2);
+    PQType<Application> queue(2);
+    Application application;
+    Application applicantOutput;
     Applicant applicant;
-    Applicant applicantOutput;
 
-    // Add applicant to the queue
-    applicant.Initialize(10);
-    queue.Enqueue(applicant);
+    // Add application to the queue
+    application.Initialize(applicant);
+    queue.Enqueue(application);
 
-    // Dequeue applicant
+    // Dequeue application
     queue.Dequeue(applicantOutput);
 
     // Test empty Queue
     assert(queue.IsEmpty());
 
-    // First applicant out should be the one inserted in an empty queue
-    assert(applicant.ComparedTo(applicantOutput) == EQUAL);
+    // First application out should be the one inserted in an empty queue
+    assert(application == applicantOutput);
 }
 
 /*
  * Test general dequeue operation so that priority order is maintained
  */
 void TestDequeueGeneral() {
-    PQType<Applicant> queue(5);
+    PQType<Application> queue(5);
+    Application application;
+    Application david;
+    Application jeff;
     Applicant applicant;
-    Applicant david;
-    Applicant jeff;
 
     // Add applicants to the queue
     // Note: jeff and david applicants 'variables'
-    jeff.Initialize(10);
+    applicant.Initialize(2, "jeff", 18, false, 0);
+    jeff.Initialize(applicant);
     queue.Enqueue(jeff);
-    applicant.Initialize(5);
-    queue.Enqueue(applicant);
-    applicant.Initialize(8);
-    queue.Enqueue(applicant);
-    david.Initialize(15);
+
+    applicant.Initialize(2, "test 1", 17, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    applicant.Initialize(2, "test 2", 14, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    applicant.Initialize(2, "david", 25, false, 0);
+    david.Initialize(applicant);
     queue.Enqueue(david);
-    applicant.Initialize(3);
-    queue.Enqueue(applicant);
+
+    applicant.Initialize(2, "test 3", 15, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
 
     // Dequeue two applicants: should be david & jeff
-    queue.Dequeue(applicant);
-    assert(applicant.ComparedTo(david) == EQUAL);
-    queue.Dequeue(applicant);
-    assert(applicant.ComparedTo(jeff) == EQUAL);
+    queue.Dequeue(application);
+    assert(application == david);
+    queue.Dequeue(application);
+    assert(application == jeff);
 
     // Check if priority is maintained in the data structure
     assert(TestPriorityMaintained(queue));
@@ -130,45 +156,63 @@ void TestDequeueGeneral() {
  * Test combinations of insert and deletes and that priority is maintained
  */
 void TestGeneralInsertAndDelete() {
-    PQType<Applicant> queue(5);
+    PQType<Application> queue(5);
+    Application application;
     Applicant applicant;
 
     // Add/Remove applicants to/from the queue
-    applicant.Initialize(5);
-    queue.Enqueue(applicant);
-    applicant.Initialize(8);
-    queue.Enqueue(applicant);
+    applicant.Initialize(3, "test 1", 15, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
 
-    queue.Dequeue(applicant);
+    applicant.Initialize(3, "test 2 priority", 15, false, 1);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    queue.Dequeue(application);
+
     assert(TestPriorityMaintained(queue));
 
-    applicant.Initialize(8);
-    queue.Enqueue(applicant);
+    applicant.Initialize(3, "test 3 priority", 15, false, 1);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
     assert(TestPriorityMaintained(queue));
 
-    applicant.Initialize(12);
-    queue.Enqueue(applicant);
-    applicant.Initialize(1);
-    queue.Enqueue(applicant);
-    queue.Dequeue(applicant);
-    queue.Dequeue(applicant);
-    queue.Dequeue(applicant);
+    applicant.Initialize(3, "test 4 priority", 26, true, 1);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    applicant.Initialize(3, "test 5", 24, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    queue.Dequeue(application);
+    queue.Dequeue(application);
+    queue.Dequeue(application);
+
     assert(TestPriorityMaintained(queue));
 }
 /*
  * Test queue's MakeEmpty and check that Dequeue of empty queue throws EmptyPQ exception
  */
 void TestMakeEmpty() {
-    PQType<Applicant> queue(3);
+    PQType<Application> queue(3);
+    Application application;
     Applicant applicant;
 
     // Add applicants to the queue
-    applicant.Initialize(10);
-    queue.Enqueue(applicant);
-    applicant.Initialize(5);
-    queue.Enqueue(applicant);
-    applicant.Initialize(8);
-    queue.Enqueue(applicant);
+    applicant.Initialize(4, "test 1 priority", 26, true, 1);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    applicant.Initialize(4, "test 2", 18, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
+
+    applicant.Initialize(4, "test 3", 19, false, 0);
+    application.Initialize(applicant);
+    queue.Enqueue(application);
 
     // Test empty Queue
     assert(!queue.IsEmpty());
@@ -177,10 +221,9 @@ void TestMakeEmpty() {
 
     // Check empty queue throws exception
     try {
-        queue.Dequeue(applicant);
+        queue.Dequeue(application);
         assert(false);
     } catch (EmptyPQ ex) {
         assert(true);
     }
 }
-
