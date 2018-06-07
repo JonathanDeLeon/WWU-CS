41c49fe13d233d6ad9e0b77fb0b45a9732027cea
Jonathan De Leon <jondeleon200@gmail.com>
2018-06-06 18:50:52 -0700

    Moving Connect4 AI project to its own repository

--- connect-four/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# Jonathan De Leon && Ethan Beaver
-# CPTR 430 Artificial Intelligence
-# Final Project
-# June 6, 2018
-#
-# Problem:
-# Create a Connect Four game and implement an AI bot that uses minimax algorithm with alpha-beta pruning
