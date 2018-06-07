024f811815bad53f53559c79fbec70a85cf33afc
Jonathan De Leon <jondeleon200@gmail.com>
2018-06-06 17:25:54 -0700

    Init README

---
+++ connect-four/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Jonathan De Leon && Ethan Beaver
+# CPTR 430 Artificial Intelligence
+# Final Project
+# June 6, 2018
+#
+# Problem:
+# Create a Connect Four game and implement an AI bot that uses minimax algorithm with alpha-beta pruning
