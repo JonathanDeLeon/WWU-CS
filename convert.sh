a3e1d73122804f58c91bad7e8abe9a055205c352
Jonathan De Leon <jonathan@campgroundautomation.com>
2018-11-26 21:47:03 -0800

    Update script

--- convert.sh
+++ convert.sh
@@ -8,10 +8,10 @@ git fetch other
 git checkout -b ${REPO} other/master
 mkdir ${REPO}
 git mv ./* ${REPO} -k                      # repeat as necessary for each file/dir
-git commit -m "Added ${REPO}"
-git checkout master
-git merge ${REPO} --allow-unrelated-histories --commit   # should add ZZZ/ to master
-git commit
-git remote rm other
-git branch -d ${REPO}                           # to get rid of the extra branch before pushing
+#git commit -m "Added ${REPO}"
+#git checkout master
+#git merge ${REPO} --allow-unrelated-histories --commit   # should add ZZZ/ to master
+#git commit
+#git remote rm other
+#git branch -d ${REPO}                           # to get rid of the extra branch before pushing
 #git push                                    # if you have a remote, that is
