4854fcdca362ce2e87ed63637b3af4deaa87fbe6
Jonathan De Leon <jonathan@campgroundautomation.com>
2018-11-26 22:00:35 -0800

    Update script

--- convert.sh
+++ convert.sh
@@ -9,8 +9,8 @@ git checkout -b ${REPO} other/master
 mkdir ${REPO}
 git mv ./* ${REPO} -k                      # repeat as necessary for each file/dir
 git commit -m "Added ${REPO}"
-#git checkout master
-#git merge ${REPO} --allow-unrelated-histories --commit   # should add ZZZ/ to master
+git checkout master
+git merge ${REPO} --allow-unrelated-histories --commit   # should add ZZZ/ to master
 #git commit
 #git remote rm other
 #git branch -d ${REPO}                           # to get rid of the extra branch before pushing
